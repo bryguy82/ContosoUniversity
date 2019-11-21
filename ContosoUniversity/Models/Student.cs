574b3fd01f0eb909ff74835befba2a3ba7c7cc87
Bryan Anderson <bryguy82@hotmail.com>
2019-11-21 02:24:15 -0300

    Edit page altered

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Display(Name = "Last Name"), Required]
         public string LastName { get; set; }
+
+        [Display(Name = "First and Middle Name"), Required]
         public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date"), DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
