b70af87824d0175ceed4690b971f5291fc65e8bd
Bryan Anderson <bryguy82@hotmail.com>
2019-11-21 01:58:16 -0300

    Adding files

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+/*using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;*/
+
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        /* public SchoolContext() : base("SchoolContext")
+        {
+        }*/
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+    }
+}
